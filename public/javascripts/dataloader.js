900c4235700e45fde083e1f7703f99435dd96012
HENAFF <theo.henaff@hotmail.fr>
2021-11-19 11:54:26 +0100

    add dataloader.js to load each graphics with the same data object add testdata.json to show the required the data scheme to build the TreemapObject, the children is VERY important

---
+++ public/javascripts/dataloader.js
@@ -0,0 +1,64 @@
+// get the data
+var data = []
+d3.csv("/data/state_fips@2.csv").then(function(d){data = d.map(d => ({
+    code: d.stusps,
+    state: d.stname,
+    value: 0,
+    city:{},
+    shooting:{}}))});
+
+d3.csv("/data/shootings.csv").then(function(dsh) {
+    // format the data
+    dsh.sort((a,b) => (a.state > b.state) ? 1 : ((b.state > a.state) ? -1 : 0)).forEach(function (d) {
+        var i = data.findIndex(x => x.code === d.state);
+        data[i].value = data[i].value+1;
+        if (!data[i].city[d.city]) {
+            data[i].city[d.city] = 0;
+        }
+        data[i].city[d.city] = data[i].city[d.city]+1;
+        data[i].shooting[d.date] = d.race;
+    });
+
+}).then(function(){
+    var map = [[0, 0, ""], [1, 0, ""], [2, 0, ""], [3, 0, ""], [4, 0, ""], [5, 0, ""], [6, 0, ""], [7, 0, ""], [8, 0, ""],
+        [9, 0, ""], [10, 0, ""], [11, 0, "ME"], [0, 1, "AK"], [1, 1, ""], [2, 1, ""], [3, 1, ""], [4, 1, ""], [5, 1, ""],
+        [6, 1, "WI"], [7, 1, ""], [8, 1, ""], [9, 1, ""], [10, 1, "VT"], [11, 1, "NH"], [0, 2, ""], [1, 2, "WA"],
+        [2, 2, "ID"], [3, 2, "MT"], [4, 2, "ND"], [5, 2, "MN"], [6, 2, "IL"], [7, 2, "MI"], [8, 2, ""], [9, 2, "NY"],
+        [10, 2, "MA"], [11, 2, ""], [0, 3, ""], [1, 3, "OR"], [2, 3, "NV"], [3, 3, "WY"], [4, 3, "SD"], [5, 3, "IA"],
+        [6, 3, "IN"], [7, 3, "OH"], [8, 3, "PA"], [9, 3, "NJ"], [10, 3, "CT"], [11, 3, "RI"], [0, 4, ""], [1, 4, "CA"],
+        [2, 4, "UT"], [3, 4, "CO"], [4, 4, "NE"], [5, 4, "MO"], [6, 4, "KY"], [7, 4, "WV"], [8, 4, "VA"], [9, 4, "MD"],
+        [10, 4, "DE"], [11, 4, ""], [0, 5, ""], [1, 5, ""], [2, 5, "AZ"], [3, 5, "NM"], [4, 5, "KS"], [5, 5, "AR"],
+        [6, 5, "TN"], [7, 5, "NC"], [8, 5, "SC"], [9, 5, "DC"], [10, 5, ""], [11, 5, ""], [0, 6, "HI"], [1, 6, ""],
+        [2, 6, ""], [3, 6, ""], [4, 6, "OK"], [5, 6, "LA"], [6, 6, "MS"], [7, 6, "AL"], [8, 6, "GA"], [9, 6, ""],
+        [10, 6, ""], [11, 6, ""], [0, 7, ""], [1, 7, ""], [2, 7, ""], [3, 7, ""], [4, 7, "TX"], [5, 7, ""],
+        [6, 7, ""], [7, 7, ""], [8, 7, ""], [9, 7, "FL"], [10, 7, ""], [11, 7, ""]]
+        .map(d => ({ col: d[0], row: d[1], code: d[2] }));
+
+    // set the dimensions and margins of the graph
+    var margin_map = {top: 20, right: 20, bottom: 30, left: 40},
+        width_map = 800 - margin_map.left - margin_map.right,
+        height_map = 450 - margin_map.top - margin_map.bottom;
+
+    const svg = d3.select("#map").append("svg")
+        .attr("font-size", "09pt")
+        .attr("width", width_map + margin_map.left + margin_map.right)
+        .attr("height", height_map + margin_map.top + margin_map.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin_map.left + "," + margin_map.top + ")");
+
+    const gmap = new GridMap(svg, width_map, height_map)
+        .size([width_map, height_map])
+        .cellPalette(d3.interpolateReds)
+        .style({sizeByValue: false, legendTitle: "Nombre de personnes tués par la police", defaultTextColor:"black"})
+        .field({ code: "code", name: "state", total: "value" })
+        .mapGrid(map)
+        .data(data)
+        .render();
+
+    svg.node();
+
+});
+
+d3.json("/data/testdata.json").then(function(dataTree) {
+    TreemapObject(dataTree);
+});
--- public/javascripts/treemap.js
+++ public/javascripts/treemap.js
@@ -1,54 +1,50 @@
-// set the dimensions and margins of the graph
-const margin_treemap = {top: 10, right: 10, bottom: 10, left: 10},
-width_treemap = 445 - margin_treemap.left - margin_treemap.right,
-height_treemap = 445 - margin_treemap.top - margin_treemap.bottom;
+function TreemapObject (data) {
+    // set the dimensions and margins of the graph
+    const margin = {top: 10, right: 10, bottom: 10, left: 10},
+        width = 445 - margin.left - margin.right,
+        height = 445 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-const treemap_svg = d3.select("#treemap")
-.append("svg")
-.attr("width", width_treemap + margin_treemap.left + margin_treemap.right)
-.attr("height", height_treemap + margin_treemap.top + margin_treemap.bottom)
-.append("g")
-.attr("transform",
-`translate(${margin_treemap.left}, ${margin_treemap.top})`);
-
+    // append the svg object to the body of the page
+    const svg = d3.select("#treemap")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            `translate(${margin.left}, ${margin.top})`);
 // Read data
-d3.csv('https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_hierarchy_1level.csv').then(function(data) {
 
-// stratify the data: reformatting for d3.js
-const root = d3.stratify()
-.id(function(d) { return d.name; })   // Name of the entity (column name is name in csv)
-.parentId(function(d) { return d.parent; })   // Name of the parent (column name is parent in csv)
-(data);
-root.sum(function(d) { return +d.value })   // Compute the numeric value for each entity
+    // read json data
+    // d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_dendrogram_full.json").then(function(data) {
+    // Give the data to this cluster layout:
+    const root = d3.hierarchy(data).sum(function(d){ return d.value}) // Here the size of each leave is given in the 'value' field in input data
 
-// Then d3.treemap computes the position of each element of the hierarchy
-// The coordinates are added to the root object above
-d3.treemap()
-.size([width_treemap, height_treemap])
-.padding(4)
-(root)
+    // Then d3.treemap computes the position of each element of the hierarchy
+    d3.treemap()
+        .size([width, height])
+        .padding(2)
+        (root)
 
-// use this information to add rectangles:
-treemap_svg
-.selectAll("rect")
-.data(root.leaves())
-.join("rect")
-.attr('x', function (d) { return d.x0; })
-.attr('y', function (d) { return d.y0; })
-.attr('width', function (d) { return d.x1 - d.x0; })
-.attr('height', function (d) { return d.y1 - d.y0; })
-.style("stroke", "black")
-.style("fill", "#69b3a2");
+    // use this information to add rectangles:
+    svg
+        .selectAll("rect")
+        .data(root.leaves())
+        .join("rect")
+        .attr('x', function (d) { return d.x0; })
+        .attr('y', function (d) { return d.y0; })
+        .attr('width', function (d) { return d.x1 - d.x0; })
+        .attr('height', function (d) { return d.y1 - d.y0; })
+        .style("stroke", "black")
+        .style("fill", "slateblue")
 
-// and to add the text labels
-treemap_svg
-.selectAll("text")
-.data(root.leaves())
-.join("text")
-.attr("x", function(d){ return d.x0+10})    // +10 to adjust position (more right)
-.attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
-.text(function(d){ return d.data.name})
-.attr("font-size", "15px")
-.attr("fill", "white")
-})
+    // and to add the text labels
+    svg
+        .selectAll("text")
+        .data(root.leaves())
+        .join("text")
+        .attr("x", function(d){ return d.x0+5})    // +10 to adjust position (more right)
+        .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
+        .text(function(d){ return d.data.name })
+        .attr("font-size", "15px")
+        .attr("fill", "white")
+};
