e8217b6d5e18a62cef65605dfe541d72dd49414c
Theo-HENAFF <theo.henaff@hotmail.fr>
2022-03-23 22:03:32 +0100

    prepare for trouble and make it double

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ let router = express.Router();
 router.get('/', function (req, res, next) {
     res.render('index', {});
 });
+router.get('/police', function (req, res, next) {
+    res.render('police.ejs', {});
+});
+router.get('/folder', function (req, res, next) {
+    res.render('folder.ejs', {});
+});
 
 module.exports = router;
